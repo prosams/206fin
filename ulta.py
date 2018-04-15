c0ee8ecab61a9952f2b373aec287a7ddb30c2923
Sam Lu <ulmas@umich.edu>
2018-04-15 18:39:26 -0400

    ok made changes so that the prim/for key is a number

--- ulta.py
+++ ulta.py
@@ -53,42 +53,46 @@ def getAllProdType(kind): #takes in eyes, lips, face, tools
 	soup = BeautifulSoup(page, 'html.parser')
 	elements = soup.find_all(class_ = "productQvContainer")
 
-	if kind == "eyes":
-		category = "Eye"
+	if kind == "eyes": #gets the numbers that will differentiate the categories
+		category = "1"
 	elif kind == "lips":
-		category = "Lip"
+		category = "2"
 	elif kind == "face":
-		category = "Face"
+		category = "3"
 	elif kind == "tools":
-		category = "Tool"
+		category = "4"
 
 	for x in elements:
-		# print("++++++++")
 		roughDesc = x.find(class_="prod-desc")
-		finDesc = roughDesc.text.strip() # basically product name
+		finDesc = roughDesc.text.strip() # ******** basically product name
 		print(finDesc)
-
 		titlerough = x.find(class_ = "prod-title")
-		finTit = titlerough.text.strip() # product brand
-
-		urlDetails = x.find('a', href = True)["href"] # this should be the indiv product url
+		finTit = titlerough.text.strip() # ********* product brand
+		urlDetails = x.find('a', href = True)["href"] # *******this should be the indiv product url
 		finalurl = "https://www.ulta.com" + urlDetails
 
 		try:
 			pricerough = x.find(class_ = "regPrice")
-			finPrice = pricerough.text.strip()
+			strippedprice = pricerough.text.strip()
+			if len(strippedprice) > 7:
+				splitprice = strippedprice.split("-")
+				moreexpensive = splitprice[-1]
+				finPrice = moreexpensive[2:]
+			else:
+				finPrice = strippedprice[1:]
 			Sale = "No"
-			# print("Not on Sale")
 		except:
 			pricerough = x.find(class_ = "pro-new-price")
-			finPrice = pricerough.text.strip()
+			strippedprice = pricerough.text.strip()
+			if len(strippedprice) > 7:
+				splitprice = strippedprice.split("-")
+				moreexpensive = splitprice[-1]
+				finPrice = moreexpensive[2:]
+			else:
+				finPrice = strippedprice[1:]
 			Sale = "Yes"
-			# print("On Sale")
 
-		#-------------- specific page crawl starts here ----------------
-
-		# second = requests.get(finalurl)
-		# secondtext = second.text
+		#---------------------- specific page crawl starts here --------------------------
 		secondtext = cacheRequest(finalurl)
 		soup = BeautifulSoup(secondtext, 'html.parser')
 
@@ -101,41 +105,30 @@ def getAllProdType(kind): #takes in eyes, lips, face, tools
 
 		try:
 			itemsize = proditem.find(id = "itemSize")
-			# sizefinal = itemsize.text.strip()
-			# itemdim = proditem.find(id = "itemSizeUOM")
-			# dimfinal = itemdim.text.strip()
 			sizeNdim = itemsize.text.strip()
 		except:
 			sizeNdim = None
-		# print(sizeNdim)
 
 		try:
 			percentrecommend = soup.find(class_ = "pr-snapshot-consensus-value pr-rounded")
 			percentrec = percentrecommend.text.strip()
 			finalpercentrec = percentrec[:-1]
-			# print(finalpercentrec)
 		except:
 			finalpercentrec = None
-			# print(finalpercentrec)
 
 		try:
 			numrevclass = soup.find(id = "rws_cnts")
 			numreview = numrevclass.text.strip()
 			numreviewfinal = numreview[1:-1]
-			# print(numreviewfinal)
 		except:
 			numreviewfinal = None
-			# print("there are no reviews")
 
 		try:
 			starrate = soup.find(class_ = "pr-rating pr-rounded average")
 			starfinal = starrate.text.strip()
 		except:
 			starfinal = None
-			# print("there are no star ratings")
-		# print(finalpercentrec + " would recommend")
-		# print(numreviewfinal + " reviews")
-		# print(starfinal + " stars")
+
 		ultimateTuple = (finDesc, finTit, category, finPrice, finalprodnum, sizeNdim, finalpercentrec, numreviewfinal, starfinal, Sale, finalurl)
 		returnlist.append(ultimateTuple)
 		# print(ultimateTuple)
@@ -152,12 +145,11 @@ def JsonFileCreator():
 	facetup = facelist
 	toolist = getAllProdType("tools")
 	tooltup = toolist
-
 	allprodDict = {}
-	allprodDict["Eye"] = eyetup
-	allprodDict["Lip"] = liptup
-	allprodDict["Face"] = facetup
-	allprodDict["Tool"] = tooltup
+	allprodDict["1"] = eyetup
+	allprodDict["2"] = liptup
+	allprodDict["3"] = facetup
+	allprodDict["4"] = tooltup
 	megalist = eyetup + liptup + facetup + tooltup
 	dumped = json.dumps(megalist)
 	fw = open("allprodlist.json","w")
@@ -217,6 +209,7 @@ def fillthings():
 	myfile = open("allprodlist.json", 'r')
 	data = myfile.read()
 	loaded = json.loads(data) # this should be a list of lists
+
 	for x in loaded:
 		insertion = (None, x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10])
 		statement = 'INSERT INTO "Products"'
@@ -234,22 +227,23 @@ def fillthings():
 	lipbrandlist = []
 	lipprodlist = []
 	for x in loaded:
-		if x[2] == "Eye":
+		if x[2] == "1":
 			eyeprodlist.append(x[0])
 			if x[1] not in eyebrandlist and not None:
 				eyebrandlist.append(x[1])
-		elif x[2] == "Tool":
-			toolprodlist.append(x[0])
-			if x[1] not in toolbrandlist and not None:
-				toolbrandlist.append(x[1])
-		elif x[2] == "Face":
-			faceprodlist.append(x[0])
-			if x[1] not in facebrandlist and not None:
-				facebrandlist.append(x[1])
-		elif x[2] == "Lip":
+		elif x[2] == "2":
 			lipprodlist.append(x[0])
 			if x[1] not in lipbrandlist and not None:
 				lipbrandlist.append(x[1])
+		elif x[2] == "3":
+			faceprodlist.append(x[0])
+			if x[1] not in facebrandlist and not None:
+				facebrandlist.append(x[1])
+		elif x[2] == "4":
+			toolprodlist.append(x[0])
+			if x[1] not in toolbrandlist and not None:
+				toolbrandlist.append(x[1])
+
 	eyetup = ("Eye", len(eyebrandlist), len(eyeprodlist))
 	liptup = ("Lip", len(lipbrandlist), len(lipprodlist))
 	facetup = ("Face", len(facebrandlist), len(faceprodlist))
@@ -263,7 +257,6 @@ def fillthings():
 		conn.commit()
 
 def process_command():
-
 	DB_NAME = 'ultadata.db'
 	try:
 		conn = sqlite3.connect(DB_NAME)
@@ -309,7 +302,7 @@ def process_command():
 	data = [trace1]
 
 	layout = go.Layout(
-				title="sam is hot",
+				title="testgraph",
 				xaxis = dict(
 				range=len(plotlytuplist)
 			),
@@ -323,7 +316,56 @@ def process_command():
 	fig = go.Figure(data=data, layout=layout)
 	py.plot(fig, filename = 'ulta-line')
 
-#JsonFileCreator()
-#init_db(DBNAME)
+def avbrand(): # this function sorts by the average star rating of a brand
+	DB_NAME = 'ultadata.db'
+	try:
+		conn = sqlite3.connect(DB_NAME)
+		cur = conn.cursor()
+	except Error as e:
+		print(e)
+	query = "SELECT * FROM 'products'"
+	cur.execute(query)
+
+	basic_statement = '''
+	SELECT Brand, AVG(StarRating)
+	FROM Products
+	JOIN Categories
+	ON Categories.Category = Products.Category
+	GROUP BY Brand
+	ORDER BY AVG(StarRating) DESC
+	'''
+	cur.execute(basic_statement)
+	plotlytuplist = []
+	for row in cur:
+		try:
+			pair = (row[0], round(float(row[1]), 1)) #this rounds like in project 3
+			plotlytuplist.append(pair)
+		except:
+			continue
+	# print(plotlytuplist)
+
+	trace1 = go.Bar(
+		x=[x[0] for x in plotlytuplist],
+		y=[x[1] for x in plotlytuplist]
+		)
+	data = [trace1]
+
+	layout = go.Layout(
+				title = "Average Star Rating for Brands Overall",
+				xaxis = dict(
+				range = len(plotlytuplist)
+			),
+				yaxis = dict(
+				range = [0, 5]
+			),
+				height=500,
+				width=1000
+		)
+
+	fig = go.Figure(data=data, layout=layout)
+	py.plot(fig, filename = 'ulta-bar')
+
+JsonFileCreator()
+init_db(DBNAME)
 #fillthings()
-process_command()
+# process_command()
