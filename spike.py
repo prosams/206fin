ea822e7a33950009bc9670b1f0e668d0e75ee341
Sam Lu <ulmas@umich.edu>
2018-04-15 17:59:07 -0400

    okay made one more plotly

--- spike.py
+++ spike.py
@@ -4,6 +4,8 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import sqlite3
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 # eyelist = getAllProdType("eyes")
 # eyetup = eyelist
@@ -27,114 +29,224 @@ import sqlite3
 #
 # print(allprodDict)
 
-DBNAME = "ultadata.db"
-def init_db(x):
-	conn = sqlite3.connect(DBNAME)
-	cur = conn.cursor()
-	statement = '''
-			DROP TABLE IF EXISTS 'Products';
-		'''
-	cur.execute(statement)
-	conn.commit()
-	statement1 = '''
-		CREATE TABLE 'Products' (
-			'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-			'Name' TEXT NOT NULL,
-			'Brand' TEXT NOT NULL,
-			'Category' TEXT NOT NULL,
-			'Cost' REAL NOT NULL,
-			'ItemNum' TEXT,
-			'ItemSizeOZ' REAL,
-			'PercentRec' REAL,
-			'Reviews' INTEGER,
-			'StarRating' REAL,
-			'Sale' TEXT NOT NULL,
-			'Url' TEXT NOT NULL
-			);
-		'''
-	cur.execute(statement1)
-	conn.commit()
+# DBNAME = "ultadata.db"
+# def init_db(x):
+# 	conn = sqlite3.connect(DBNAME)
+# 	cur = conn.cursor()
+# 	statement = '''
+# 			DROP TABLE IF EXISTS 'Products';
+# 		'''
+# 	cur.execute(statement)
+# 	conn.commit()
+# 	statement1 = '''
+# 		CREATE TABLE 'Products' (
+# 			'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+# 			'Name' TEXT NOT NULL,
+# 			'Brand' TEXT NOT NULL,
+# 			'Category' TEXT NOT NULL,
+# 			'Cost' REAL NOT NULL,
+# 			'ItemNum' TEXT,
+# 			'ItemSizeOZ' REAL,
+# 			'PercentRec' REAL,
+# 			'Reviews' INTEGER,
+# 			'StarRating' REAL,
+# 			'Sale' TEXT NOT NULL,
+# 			'Url' TEXT NOT NULL
+# 			);
+# 		'''
+# 	cur.execute(statement1)
+# 	conn.commit()
+#
+# 	statement2 = '''
+# 			DROP TABLE IF EXISTS 'Categories';
+# 		'''
+# 	cur.execute(statement2)
+# 	conn.commit()
+#
+# 	conn = sqlite3.connect(DBNAME)
+# 	cur = conn.cursor()
+# 	statement3 = '''
+# 		CREATE TABLE 'Categories' (
+# 			'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+# 			'Category' TEXT NOT NULL,
+# 			'BrandsTotalNum' TEXT NOT NULL,
+# 			'ProductsTotalNum' TEXT NOT NULL
+# 			);
+# 		'''
+# 	cur.execute(statement3)
+# 	conn.commit()
+#
+# # init_db(DBNAME)
+#
+# def fillthings():
+# 	conn = sqlite3.connect("ultadata.db")
+# 	cur = conn.cursor()
+#
+# 	myfile = open("allprodlist.json", 'r')
+# 	data = myfile.read()
+# 	loaded = json.loads(data) # this should be a list of lists
+#
+# 	for x in loaded:
+# 		insertion = (None, x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10])
+# 		statement = 'INSERT INTO "Products"'
+# 		statement += 'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+# 		cur.execute(statement, insertion)
+# 		# at the end of this hopefully the products table has been filled
+# 		conn.commit()
+#
+# 	eyebrandlist = []
+# 	eyeprodlist = []
+# 	toolbrandlist = []
+# 	toolprodlist = []
+# 	facebrandlist = []
+# 	faceprodlist = []
+# 	lipbrandlist = []
+# 	lipprodlist = []
+#
+# 	for x in loaded:
+# 		if x[2] == "Eye":
+# 			eyeprodlist.append(x[0])
+# 			if x[1] not in eyebrandlist and not None:
+# 				eyebrandlist.append(x[1])
+# 		elif x[2] == "Tool":
+# 			toolprodlist.append(x[0])
+# 			if x[1] not in toolbrandlist and not None:
+# 				toolbrandlist.append(x[1])
+# 		elif x[2] == "Face":
+# 			faceprodlist.append(x[0])
+# 			if x[1] not in facebrandlist and not None:
+# 				facebrandlist.append(x[1])
+# 		elif x[2] == "Lip":
+# 			lipprodlist.append(x[0])
+# 			if x[1] not in lipbrandlist and not None:
+# 				lipbrandlist.append(x[1])
+#
+# 	eyetup = ("Eye", len(eyebrandlist), len(eyeprodlist))
+# 	liptup = ("Lip", len(lipbrandlist), len(lipprodlist))
+# 	facetup = ("Face", len(facebrandlist), len(faceprodlist))
+# 	tooltup = ("Tool", len(toolbrandlist), len(toolprodlist))
+# 	categorytuple = (eyetup, liptup, facetup, tooltup)
+#
+# 	for tup in categorytuple:
+# 		insert = (None, tup[0], tup[1], tup[2])
+# 		statement = 'INSERT INTO "Categories" '
+# 		statement += 'VALUES (?, ?, ?, ?)'
+# 		cur.execute(statement, insert)
+# 		conn.commit()
+#
+# 	print("ok it made it here!!")
 
-	statement2 = '''
-			DROP TABLE IF EXISTS 'Categories';
-		'''
-	cur.execute(statement2)
-	conn.commit()
 
-	conn = sqlite3.connect(DBNAME)
-	cur = conn.cursor()
-	statement3 = '''
-		CREATE TABLE 'Categories' (
-			'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-			'Category' TEXT NOT NULL,
-			'BrandsTotalNum' TEXT NOT NULL,
-			'ProductsTotalNum' TEXT NOT NULL
-			);
-		'''
-	cur.execute(statement3)
-	conn.commit()
+def process_command():
+	DB_NAME = 'ultadata.db'
+	try:
+		conn = sqlite3.connect(DB_NAME)
+		cur = conn.cursor()
+	except Error as e:
+		print(e)
+	query = "SELECT * FROM 'products'"
+	cur.execute(query)
 
-# init_db(DBNAME)
+	basic_statement = '''
+	SELECT Name, starrating
+	FROM Products
+	JOIN Categories
+	ON Categories.Category = Products.Category
+	ORDER BY Products.starrating DESC
+	'''
+	strulta = str(basic_statement)
+	cur.execute(strulta)
+	plotlytuplist = []
+	for row in cur:
+		try:
+			pair = (row[0], round(float(row[1]), 1)) #this rounds like in project 3
+			plotlytuplist.append(pair)
+		except:
+			continue
+	print(plotlytuplist)
 
-def fillthings():
-	conn = sqlite3.connect("ultadata.db")
-	cur = conn.cursor()
+	trace1 = go.Scatter(
+			type='scatter',
+			x=[x[0] for x in plotlytuplist],
+			y=[x[1] for x in plotlytuplist],
+			marker=dict(
+			color=['rgb({},{},{})'.format(random.randint(0, 200), random.randint(0, 200), random.randint(0, 200)) for x in plotlytuplist],
+			size=10
+			),
+				line=dict(
+				color=['rgb({},{},{})'.format(random.randint(0, 200), random.randint(0, 200), random.randint(0, 200)) for x in plotlytuplist],
+				width = 4
+				),
+				mode='markers+lines'
+			)
+	data = [trace1]
 
-	myfile = open("allprodlist.json", 'r')
-	data = myfile.read()
-	loaded = json.loads(data) # this should be a list of lists
+	layout = go.Layout(
+				title="testgraph",
+				xaxis = dict(
+				range=len(plotlytuplist)
+			),
+				yaxis = dict(
+				range=[0, 5]
+			),
+				height=500,
+				width=1000
+		)
 
-	for x in loaded:
-		insertion = (None, x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10])
-		statement = 'INSERT INTO "Products"'
-		statement += 'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-		cur.execute(statement, insertion)
-		# at the end of this hopefully the products table has been filled
-		conn.commit()
+	fig = go.Figure(data=data, layout=layout)
+	py.plot(fig, filename = 'ulta-line')
 
-	eyebrandlist = []
-	eyeprodlist = []
-	toolbrandlist = []
-	toolprodlist = []
-	facebrandlist = []
-	faceprodlist = []
-	lipbrandlist = []
-	lipprodlist = []
+def avbrand(): # this function sorts by the average star rating of a brand
+	DB_NAME = 'ultadata.db'
+	try:
+		conn = sqlite3.connect(DB_NAME)
+		cur = conn.cursor()
+	except Error as e:
+		print(e)
+	query = "SELECT * FROM 'products'"
+	cur.execute(query)
 
-	for x in loaded:
-		if x[2] == "Eye":
-			eyeprodlist.append(x[0])
-			if x[1] not in eyebrandlist and not None:
-				eyebrandlist.append(x[1])
-		elif x[2] == "Tool":
-			toolprodlist.append(x[0])
-			if x[1] not in toolbrandlist and not None:
-				toolbrandlist.append(x[1])
-		elif x[2] == "Face":
-			faceprodlist.append(x[0])
-			if x[1] not in facebrandlist and not None:
-				facebrandlist.append(x[1])
-		elif x[2] == "Lip":
-			lipprodlist.append(x[0])
-			if x[1] not in lipbrandlist and not None:
-				lipbrandlist.append(x[1])
+	basic_statement = '''
+	SELECT Brand, AVG(StarRating)
+	FROM Products
+	JOIN Categories
+	ON Categories.Category = Products.Category
+	GROUP BY Brand
+	ORDER BY AVG(StarRating) DESC
+	'''
+	cur.execute(basic_statement)
+	plotlytuplist = []
+	for row in cur:
+		try:
+			pair = (row[0], round(float(row[1]), 1)) #this rounds like in project 3
+			plotlytuplist.append(pair)
+		except:
+			continue
+	# print(plotlytuplist)
 
-	eyetup = ("Eye", len(eyebrandlist), len(eyeprodlist))
-	liptup = ("Lip", len(lipbrandlist), len(lipprodlist))
-	facetup = ("Face", len(facebrandlist), len(faceprodlist))
-	tooltup = ("Tool", len(toolbrandlist), len(toolprodlist))
-	categorytuple = (eyetup, liptup, facetup, tooltup)
+	trace1 = go.Bar(
+		x=[x[0] for x in plotlytuplist],
+		y=[x[1] for x in plotlytuplist]
+		)
+	data = [trace1]
 
-	for tup in categorytuple:
-		insert = (None, tup[0], tup[1], tup[2])
-		statement = 'INSERT INTO "Categories" '
-		statement += 'VALUES (?, ?, ?, ?)'
-		cur.execute(statement, insert)
-		conn.commit()
+	layout = go.Layout(
+				title = "Average Star Rating for Brands Overall",
+				xaxis = dict(
+				range = len(plotlytuplist)
+			),
+				yaxis = dict(
+				range = [0, 5]
+			),
+				height=500,
+				width=1000
+		)
 
-	print("ok it made it here!!")
+	fig = go.Figure(data=data, layout=layout)
+	py.plot(fig, filename = 'ulta-bar')
 
-fillthings()
+avbrand()
+# process_command()
 
 # eyereq = cacheRequest("https://www.ulta.com/makeup-eyes?N=26yd&No=0&Nrpp=1000")
 # # this is because max numbers of products on a page is 1000 but there are like
